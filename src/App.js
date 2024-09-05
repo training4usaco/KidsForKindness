d27d8b08328a25814eec67c2ce4ce9c06716e168
training4usaco <103396810+training4usaco@users.noreply.github.com>
2024-09-04 23:28:57 -0700

    Setup file structure + added home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import { Footer, Blog, Possibility, Features, WhatKFK, Header } from './containers';
+import { CTA, Navbar } from './components';
+import './App.css'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header__container">
+        <Navbar />
+        <Header />
+      </div>
+      <WhatKFK />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
